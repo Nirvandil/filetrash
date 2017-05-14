e5b86dadf0f572e777b42bad03153a9ad1bd04ae
nirvandil <nirvandil@gmail.com>
2017-05-14 19:36:36 +0300

    Start implementing logging with AOP

---
+++ src/cf/nirvandil/filetrash/aop/LoggingAop.java
@@ -0,0 +1,31 @@
+package cf.nirvandil.filetrash.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by Vladimir Sukharev aka Nirvandil on 14.05.17 at 17:29.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+@Aspect
+public class LoggingAop
+{
+    private Logger logger = Logger.getLogger("cf.nirvandil.filetrash.controller");
+
+    @Before("execution(* cf.nirvandil.filetrash.controller.TrashMainController.uploadFile(..))")
+    public void logStartingUpload()
+    {
+        logger.warning("Test for before writing file!");
+    }
+}
--- src/cf/nirvandil/filetrash/controller/TrashMainController.java
+++ src/cf/nirvandil/filetrash/controller/TrashMainController.java
@@ -14,6 +14,7 @@ import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.logging.Logger;
 
 @Controller
 public class TrashMainController implements ServletContextAware
